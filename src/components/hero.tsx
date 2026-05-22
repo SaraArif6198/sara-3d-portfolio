1772a7bea39901a153759f42a34980cd4050c927
Sara Arif <saraarif6198@gmail.com>
2026-05-23 02:18:36 +0500

    Fix mobile Safari tap bug by overriding pointer-events on mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from "./canvas";
 
 export const Hero = () => (
   <section className="relative w-full h-screen mx-auto">
-    <div className={`absolute inset-0 top-[90px] z-10 pointer-events-none max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+    <div className={`absolute inset-0 top-[90px] z-10 sm:pointer-events-none pointer-events-auto max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
       <div className="flex flex-col justify-center items-center mt-5">
         <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
         <div className="w-1 sm:h-80 h-40 violet-gradient" />
